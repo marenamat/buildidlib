6ba4c2488e43bb988cf4a5bb86d1801ffc15d598
Maria Matejka <mq@ucw.cz>
2025-06-03 09:49:55 +0200

    Fix debian lists to actually download and parse

--- tools/debian-packages.py
+++ tools/debian-packages.py
@@ -1,7 +1,6 @@
+import gzip
 import requests
 import sys
-
-r = requests.get(sys.argv[1], stream=True)
 
 class DebianPackageManifest:
     def __init__(self):
@@ -9,6 +8,7 @@ class DebianPackageManifest:
         self.info = {}
 
     def line(self, line):
+#        print(line)
         if line.startswith(" "):
             assert(self.last is not None)
             self.info[self.last] += line
@@ -16,6 +16,7 @@ class DebianPackageManifest:
             k, v = line.split(": ", 1)
             assert(k not in self.info)
             self.info[k] = v
+            self.last = k
 
     def done(self):
         if "Build-Ids" in self.info:
@@ -23,15 +24,19 @@ class DebianPackageManifest:
                 print(f"BID;{f};{self.info['SHA256']}")
             print(f"SHA;{self.info['SHA256']};{self.info['Package']};{self.info['Version']}")
 
-cur = DebianPackageManifest()
-for line in r.iter_lines():
-    if line:
-        continue
+r = requests.get(sys.argv[1], stream=True)
+g = gzip.open(r.raw, "r")
 
-    if line == "":
+cur = DebianPackageManifest()
+for lb in g:
+    line = lb.decode()
+    assert(line[-1] == "\n")
+
+    if len(line) == 1:
         cur.done()
+#        print("Done package", cur.info['Package'])
         cur = DebianPackageManifest()
     else:
-        cur.line(line)
+        cur.line(line[:-1])
 
 cur.done()
